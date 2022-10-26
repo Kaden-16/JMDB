d70b8f605497f73a7c39cc87ff9d57d83926f26c
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-26 14:57:31 -0400

    Update WatchList.java

--- src/WatchList.java
+++ src/WatchList.java
@@ -3,7 +3,7 @@ public class WatchList {
 
     private String watchList[];
     private int amountMovies = 0;
-
+    
     private void addToWatchList(String title) {
         watchList[amountMovies] = title;
         amountMovies++;
