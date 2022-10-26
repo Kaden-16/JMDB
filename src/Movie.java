61eddd438146471bdc1f168210ed330fb3c9cdd9
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-26 14:34:54 -0400

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,15 @@
 
 public class Movie {
+    private String title;
+    private int year;
+    private String director;
+    private String poster;
+    private String[] actorList;
+    private double reviewRating;
+    private String movieRating;
+    
 
+    
+    
+    
 }
