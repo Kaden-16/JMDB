426f6846b5fe200e843d4b8c5307c8fc43a0560e
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-26 14:35:51 -0400

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,15 +1,24 @@
 
 public class Movie {
+    
     private String title;
     private int year;
     private String director;
     private String poster;
     private String[] actorList;
     private double reviewRating;
+    //Might not be able to do it
     private String movieRating;
     
 
-    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
     
     
 }
