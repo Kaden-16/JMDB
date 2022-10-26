07ea1c2883ed658d8e198df4ca77cfc975c93a17
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-26 14:43:29 -0400

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -10,6 +10,17 @@ public class Movie {
     //Might not be able to do it
     private String movieRating;
     
+    public Movie(String title, String director, String poster, String[] actorList,
+            double reviewRating, String movieRating, int pages, int year) {
+        this.title = title;
+        this.director = director;
+        this.poster = poster;
+        this.actorList = actorList;
+        this.reviewRating = reviewRating;
+        this.movieRating = movieRating;
+        this.year = year;
+    }
+    
 
     public String getTitle() {
         return title;
@@ -26,7 +37,18 @@ public class Movie {
     public String[] getActorList() {
         return actorList;
     }
+    
+    public double getReviewRating() {
+        return reviewRating;
+    }
 
+    public String movieRating() {
+        return movieRating;
+    }
+    
+    public int getYear() {
+        return year;
+    }
     
     
 }
