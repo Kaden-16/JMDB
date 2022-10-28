1fd94057ea604aa2d75c45c84d577141f0b7a1d6
Arthur Schevey <arthur.schevey@gmail.com>
2022-10-28 14:36:41 -0400

    Added starter code.

--- ui/MainGUI.java
+++ ui/MainGUI.java
@@ -1,4 +1,20 @@
 
 public class MainGUI {
+    private JFrame frame;
 
+    public SwingTest() {
+        frame = new JFrame("Visual Bookz");
+    }
+
+    private void display() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingTest test = new SwingTest();
+        test.display();
+    }
 }
