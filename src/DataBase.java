227cc5d488a6079963d8610c0b951f1cb1412958
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-28 14:12:48 -0400

    Update DataBase.java

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,4 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DataBase {
 
+    public void SearchMovie(String title) throws IOException {
+        URL oracle = new URL(
+                "https://imdb-api.com/en/API/SearchMovie/k_mcx0w8k/" + title);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+    }
 }
