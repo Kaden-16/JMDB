46db2efc57f9bf6d3e210e4a5ae5e79a955b7a75
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-28 14:16:47 -0400

    Update DataBase.java

--- src/DataBase.java
+++ src/DataBase.java
@@ -7,19 +7,28 @@ import java.net.URL;
 public class DataBase {
 
     public String[] SearchMovie(String title) throws IOException {
+        String[] movieList = new String[3];
+
         URL oracle = new URL(
                 "https://imdb-api.com/en/API/SearchMovie/k_mcx0w8k/" + title);
+
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(oracle.openStream()));
-
         String inputLine;
-        
-        
-        String[] movieList = new String[5];
-        
+        int value = 0;
+        while ((inputLine = in.readLine()) != null) {
+            if (value <= 3) {
+                movieList[value] = inputLine;
+                value++;
+            } else {
+                break;
+            }
+        }
+        in.close();
+
         return movieList;
     }
-    
+
     public void SearchMovieByTitle(String title) throws IOException {
         URL oracle = new URL(
                 "https://imdb-api.com/en/API/SearchMovie/k_mcx0w8k/" + title);
