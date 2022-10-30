3339c0dc55ad2be18ddcc405cdcfb2a8cde14f64
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-30 17:20:15 -0400

    Test Cases for Database

---
+++ test/DataBaseTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class DataBaseTest {
+    
+    
+    
+    @Test
+    public void SearchMovieTest() throws Exception {
+        DataBase.SearchMovie("Inception");
+        
+    }
+    
+}
