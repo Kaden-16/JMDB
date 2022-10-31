a5324b46e0d3115f5fda801fae587010be0a7cbf
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-10-31 14:45:03 -0400

    Update DataBase.java

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
-import java.io.*;
-import java.util.*;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-
 
 public class DataBase {
 
@@ -20,21 +16,15 @@ public class DataBase {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(oracle.openStream()));
 
-        String inputLine;
 
-        while ((inputLine = in.readLine()) != null) {
-            System.out.println("\n" +inputLine);
-   
-            ObjectMapper map = new ObjectMapper();
-            
-            // now get id & value
-            String Description = (String) jsonObject.get("description");
-            String title = (String) jsonObject.get("title");
-           // movieList[i] = new Movie(title, Description);
+        ObjectMapper map = new ObjectMapper();
+        JsonNode tree = map.readTree(in);
+
+        for (int i = 0; i < tree.size(); i++) {
+            movieList[i] = new Movie(tree.get(i).get("title").asText(),
+                    tree.get(i).get("description").asText());
+            System.out.println(movieList[i]);
         }
-
-        in.close();
-
         return movieList;
     }
 
