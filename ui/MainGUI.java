f9d17a7a53e5d62da5b8eb6cfc370a4f4a70baa9
Kaden Rauch <rauchkm@dukes.jmu.edu>
2022-11-02 14:22:13 -0400

    Made new main panel

--- ui/MainGUI.java
+++ ui/MainGUI.java
@@ -16,46 +16,60 @@ import javax.swing.JTextField;
 
 public class MainGUI implements ActionListener {
     private JFrame frame;
+    private JTextField searchText;
+    private JPanel panel1;
+    private JPanel panel2;
 
 
     public MainGUI() {
         frame = new JFrame("JMU IMDB");
         searchPanel();
+        moviePanel();
         display();
     }
 
     private void display() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+       // frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
     }
     
     private JPanel searchPanel() {
-      JPanel panel = new JPanel();
-      frame.add(panel, BorderLayout.NORTH);
+      panel1 = new JPanel();
+      frame.add(panel1, BorderLayout.NORTH);
       JLabel label = new JLabel("Search");
       label.setFont(new Font("Sherif", Font.PLAIN, 17));
-      JTextField searchText = new JTextField(20);
+      searchText = new JTextField(20);
       searchText.setColumns(22);
       JButton b = new JButton();
       b.setSize(20, 20);
       b.addActionListener(this);
       Image icon = (new ImageIcon("mag.png")).getImage().getScaledInstance(b.getWidth(), b.getHeight(), Image.SCALE_SMOOTH);
       b.setIcon(new ImageIcon(icon));
-      panel.setVisible(true);
-      panel.setBackground(Color.red);
-      panel.add(label).setVisible(true);
-      panel.add(searchText).setVisible(true);
-      panel.add(b).setVisible(true);
-      return panel;
+      panel1.setVisible(true);
+      panel1.setBackground(Color.red);
+      panel1.add(label).setVisible(true);
+      panel1.add(searchText).setVisible(true);
+      panel1.add(b).setVisible(true);
+      return panel1;
+    }
+  
+    private JPanel moviePanel() {
+      panel2 = new JPanel();
+      frame.add(panel2);
+      panel2.setVisible(true);
+      panel2.setBounds(panel1.getX(), panel1.getY(), frame.getWidth(), frame.getHeight());
+      panel2.setBackground(Color.blue);
+      return panel2;
     }
     
     @Override
     public void actionPerformed(ActionEvent e)
     {
-      // TODO Auto-generated method stub
-      
+        
+        SearchBar.showSearchMovies(DataBase.SearchMovie(searchText.getText())), moviePanel());
       
     }
     
