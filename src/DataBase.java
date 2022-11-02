24e16553bc1a20670171afb43becbd22327434b3
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-11-02 14:21:51 -0400

    new update

--- src/DataBase.java
+++ src/DataBase.java
@@ -30,13 +30,4 @@ public class DataBase {
         }
         return movieList;
     }
-
-    public void SearchMovieByTitle(String title) throws IOException {
-        URL oracle = new URL(
-                "https://imdb-api.com/en/API/SearchMovie/k_mcx0w8k/" + title);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
-
-        String inputLine;
-    }
 }
