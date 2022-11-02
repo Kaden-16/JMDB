8f73fba92ded926eafd50ae2e143837c7bcbadaa
jely5 <jackson3054@gmail.com>
2022-11-02 13:29:59 -0400

    search bar and search bar test

--- test/SearchBarTest.java
+++ test/SearchBarTest.java
@@ -8,31 +8,24 @@ import javax.swing.JFrame;
 
 public class SearchBarTest {
 	
-	private JFrame frame;
 
-    public SearchBarTest() {
-        frame = new JFrame("Visual Bookz");
-    }
-
-    private void display() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    public static void main(String[] args) {
+    	JFrame frame = new JFrame("Visual Bookz");
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.pack();
         frame.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
+    	
     	Movie[] test = new Movie[3];
-    	Movie fake1 = new Movie("Inception", 1999);
-    	Movie fake2 = new Movie("Pulp Fiction", 2003);
-    	Movie fake3 = new Movie("Harry Potter", 3000);
+    	Movie fake1 = new Movie("Inception", "Chicken");
+    	Movie fake2 = new Movie("Pulp Fiction", "Tasty Burger");
+    	Movie fake3 = new Movie("Harry Potter", "Wiazard");
     	test[0] = fake1;
     	test[1] = fake2;
     	test[2] = fake3;
     	
-    	SearchBar.showSearchMovies(test);
-    	SearchBarTest gui = new SearchBarTest();
-		gui.display();
+    	SearchBar.showSearchMovies(test, frame);
 	}
     
 }
--- ui/SearchBar.java
+++ ui/SearchBar.java
@@ -1,14 +1,16 @@
 import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
-public class SearchBar {
+public class SearchBar{
 
 
-	public  static JPanel firstResult = new JPanel();
+	public  static JPanel firstResult = new JPanel(new GridLayout(2,2));
 	public  static JPanel secondResult = new JPanel();
 	public  static JPanel thirdResult= new JPanel();
 	
@@ -17,30 +19,39 @@ public class SearchBar {
 	public static JLabel thirdLabel;
 	public static Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
 	
-	public static void showSearchMovies(Movie[] results) {
+	public static void showSearchMovies(Movie[] results, JFrame frame) {
+		frame.add(firstResult);
+		frame.add(secondResult);
+		frame.add(thirdResult);
 		
-		firstResult.setBounds(200, 200, 150, 50);
+		firstResult.setBounds(500, 50, 250, 75);
 		firstResult.setBorder(blackBorder);
 		firstResult.setBackground(Color.LIGHT_GRAY);
 		String movieResultOne = String.format("%s (%s)", results[0].getTitle(), results[0].getYear());
 		firstLabel = new JLabel(movieResultOne);
 		firstLabel.setBackground(Color.blue);
+		firstLabel.setHorizontalAlignment(JLabel.CENTER);
+		firstLabel.setVerticalAlignment(JLabel.CENTER);
 		firstResult.add(firstLabel);
 		firstResult.setVisible(true);
 		
-		secondResult.setBounds(200, 250, 150, 45);
+		secondResult.setBounds(500, 150, 250, 75);
 		secondResult.setBorder(blackBorder);
 		secondResult.setBackground(Color.LIGHT_GRAY);
 		String movieResultTwo = String.format("%s (%s)", results[1].getTitle(), results[1].getYear());
 		secondLabel = new JLabel(movieResultTwo);
+		secondLabel.setHorizontalAlignment(JLabel.CENTER);
+		secondLabel.setVerticalAlignment(JLabel.CENTER);
 		secondResult.add(secondLabel);
 		secondResult.setVisible(true);
 		
-		thirdResult.setBounds(200, 300, 150, 45);
+		thirdResult.setBounds(500, 250, 250, 75);
 		thirdResult.setBorder(blackBorder);
 		thirdResult.setBackground(Color.LIGHT_GRAY);
 		String movieResultThree = String.format("%s (%s)", results[2].getTitle(), results[2].getYear());
 		thirdLabel = new JLabel(movieResultThree);
+		thirdLabel.setHorizontalAlignment(JLabel.CENTER);
+		thirdLabel.setVerticalAlignment(JLabel.CENTER);
 		thirdResult.add(thirdLabel);
 		thirdResult.setVisible(true);
 	}
