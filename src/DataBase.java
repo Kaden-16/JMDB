d358a7615413a9ca7d3e1e298c052c39c5240a41
WalkerHTodd <113936275+WalkerHTodd@users.noreply.github.com>
2022-11-02 14:45:11 -0400

    Update DataBase.java

--- src/DataBase.java
+++ src/DataBase.java
@@ -22,7 +22,8 @@ public class DataBase {
         //System.out.println(tree.toPrettyString());
         for (int i = 0; i < tree.size(); i++) {
             if(i < 3) {
-                System.out.println(tree.get("results").get(i).get("title").asText());
+            //System.out.println(tree.get("results").get(i).get("description").asText());
+
             movieList[i] = new Movie(tree.get("results").get(i).get("title").asText(),
                     tree.get("results").get(i).get("description").asText());
             } else {
@@ -30,5 +31,29 @@ public class DataBase {
             }
         }
         return movieList;
+    }
+
+    public Movie SearchMovieByTitle(String title) throws IOException {
+        URL oracle = new URL(
+                "https://imdb-api.com/en/API/SearchMovie/k_mcx0w8kk/" + title);
+
+        InputStream in = oracle.openStream();
+
+        ObjectMapper map = new ObjectMapper();
+
+        Movie finalMovie = new Movie(title, title, title, null, 0, title, 0, 0);
+        JsonNode tree = map.readTree(in);
+        //System.out.println(tree.toPrettyString());
+        for (int i = 0; i < tree.size(); i++) {
+            if(i < 3) {
+            //System.out.println(tree.get("results").get(i).get("description").asText());
+
+            //movieList[i] = new Movie(tree.get("results").get(i).get("title").asText(),
+                    //tree.get("results").get(i).get("description").asText());
+            } else {
+                break;
+            }
+        }
+        return finalMovie;
     }
 }
