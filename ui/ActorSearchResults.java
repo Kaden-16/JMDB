f4a2f6560cb4df63d069155a0c209dc2ba66d959
jely5 <jackson3054@gmail.com>
2022-12-07 14:32:11 -0500

    actor search now shows pictures

--- ui/ActorSearchResults.java
+++ ui/ActorSearchResults.java
@@ -1,47 +1,44 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 
 public class ActorSearchResults {
 
-	
 	public static JPanel showActorSearchResults(Actor[] results) throws MalformedURLException {
 		MainGUI.backButton.setVisible(false);
 		JPanel firstResult = new JPanel(new BorderLayout());
 		JPanel secondResult = new JPanel(new BorderLayout());
-		JPanel thirdResult= new JPanel(new BorderLayout());
-		JPanel fourthResult= new JPanel(new BorderLayout());
-		//JPanel fifthResult= new JPanel(new BorderLayout());
-		//JPanel sixthResult= new JPanel(new BorderLayout());
-		JPanel mainPanel = new JPanel(new GridLayout(2,2));
-		
-		
+		JPanel thirdResult = new JPanel(new BorderLayout());
+		JPanel fourthResult = new JPanel(new BorderLayout());
+		JPanel mainPanel = new JPanel(new GridLayout(2, 2));
+
 		JLabel firstLabel;
 		JLabel secondLabel;
 		JLabel thirdLabel;
 		JLabel fourthLabel;
-		//JLabel fifthLabel;
-		//JLabel sixthLabel;
 		Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
-		
-		Color purple = new Color(181,153,206);
-		
+
+		Color purple = new Color(181, 153, 206);
+
 		mainPanel.add(firstResult);
 		mainPanel.add(secondResult);
 		mainPanel.add(thirdResult);
 		mainPanel.add(fourthResult);
-		//mainPanel.add(fifthResult);
-		//mainPanel.add(sixthResult);
-		
-		
+
 		firstResult.setSize(250, 75);
 		firstResult.setBorder(blackBorder);
 		firstResult.setBackground(purple);
@@ -49,24 +46,25 @@ public class ActorSearchResults {
 		firstLabel = new JLabel(actorResultOne);
 		firstLabel.setBackground(Color.blue);
 		firstLabel.setHorizontalAlignment(JLabel.CENTER);
-		firstResult.add(firstLabel,BorderLayout.NORTH);
+		firstResult.add(firstLabel, BorderLayout.NORTH);
 		firstResult.setVisible(true);
-		
-		
-		JButton button1 = new JButton("View " + results[0].getName());
-		button1.setSize(50, 10);
-		button1.setHorizontalAlignment(JButton.CENTER);
-		firstResult.add(button1, BorderLayout.SOUTH);
-		
-		button1.addActionListener(e -> {
-			//try {
-				//MainGUI.changePanel(MovieDisplay.displayMovie(DataBase.SearchMovieByID(results[0].getID())));
-			//} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				//e1.printStackTrace();
-			//}
-		});
-		
+
+		JLabel poster = new JLabel();
+		if (results[0].getImage() != null) {
+			try {
+				URL posterLink = new URL(results[0].getImage());
+				ImageIcon posterImage = new ImageIcon(posterLink);
+				poster.setIcon(new ImageIcon(normalizeImage(posterImage)));
+				poster.setHorizontalAlignment(SwingConstants.TRAILING);
+				firstResult.add(poster, JLabel.CENTER); // CENTER DOESNT WORK
+			} catch (MalformedURLException e) {
+				Image icon = (new ImageIcon("NoImageLink.png")).getImage();
+				poster.setIcon(new ImageIcon(icon));
+				poster.setHorizontalAlignment(SwingConstants.TRAILING);
+				firstResult.add(poster, JLabel.CENTER); // CENTER DOESNT WORK
+			}
+		}
+
 		secondResult.setSize(250, 75);
 		secondResult.setBorder(blackBorder);
 		secondResult.setBackground(purple);
@@ -74,24 +72,26 @@ public class ActorSearchResults {
 		secondLabel = new JLabel(actorResultTwo);
 		secondLabel.setBackground(Color.blue);
 		secondLabel.setHorizontalAlignment(JLabel.CENTER);
-		secondResult.add(secondLabel,BorderLayout.NORTH);
+		secondResult.add(secondLabel, BorderLayout.NORTH);
 		secondResult.setVisible(true);
-		
-		
-		JButton button2 = new JButton("View " + results[1].getName());
-		button2.setSize(50, 10);
-		button2.setHorizontalAlignment(JButton.CENTER);
-		secondResult.add(button2, BorderLayout.SOUTH);
-		
-		button2.addActionListener(e -> {
-			//try {
-				//MainGUI.changePanel(MovieDisplay.displayMovie(DataBase.SearchMovieByID(results[1].getID())));
-			//} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				//e1.printStackTrace();
-			//}
-		});
-		
+
+		JLabel posterTwo = new JLabel();
+		if (results[1].getImage() != null) {
+			try {
+
+				URL posterLinkTwo = new URL(results[1].getImage());
+				ImageIcon posterImageTwo = new ImageIcon(posterLinkTwo);
+				posterTwo.setIcon(new ImageIcon(normalizeImage(posterImageTwo)));
+				posterTwo.setHorizontalAlignment(SwingConstants.TRAILING);
+				secondResult.add(posterTwo, JLabel.CENTER); // CENTER DOESNT WORK
+			} catch (MalformedURLException e) {
+				Image icon = (new ImageIcon("NoImageLink.png")).getImage();
+				posterTwo.setIcon(new ImageIcon(icon));
+				posterTwo.setHorizontalAlignment(SwingConstants.TRAILING);
+				secondResult.add(posterTwo, JLabel.CENTER); // CENTER DOESNT WORK
+			}
+		}
+
 		thirdResult.setSize(250, 75);
 		thirdResult.setBorder(blackBorder);
 		thirdResult.setBackground(purple);
@@ -99,24 +99,25 @@ public class ActorSearchResults {
 		thirdLabel = new JLabel(actorResultThree);
 		thirdLabel.setBackground(Color.blue);
 		thirdLabel.setHorizontalAlignment(JLabel.CENTER);
-		thirdResult.add(thirdLabel,BorderLayout.NORTH);
+		thirdResult.add(thirdLabel, BorderLayout.NORTH);
 		thirdResult.setVisible(true);
-		
-		
-		JButton button3 = new JButton("View " + results[2].getName());
-		button3.setSize(50, 10);
-		button3.setHorizontalAlignment(JButton.CENTER);
-		thirdResult.add(button3, BorderLayout.SOUTH);
-		
-		button3.addActionListener(e -> {
-			//try {
-				//MainGUI.changePanel(MovieDisplay.displayMovie(DataBase.SearchMovieByID(results[2].getID())));
-			//} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				//e1.printStackTrace();
-			//}
-		});
-		
+
+		JLabel posterThree = new JLabel();
+		if (results[2].getImage() != null) {
+			try {
+				URL posterLinkThree = new URL(results[2].getImage());
+				ImageIcon posterImageThree = new ImageIcon(posterLinkThree);
+				posterThree.setIcon(new ImageIcon(normalizeImage(posterImageThree)));
+				posterThree.setHorizontalAlignment(SwingConstants.TRAILING);
+				thirdResult.add(posterThree, JLabel.CENTER); // CENTER DOESNT WORK
+			} catch (MalformedURLException e) {
+				Image icon = (new ImageIcon("NoImageLink.png")).getImage();
+				posterThree.setIcon(new ImageIcon(icon));
+				posterThree.setHorizontalAlignment(SwingConstants.TRAILING);
+				thirdResult.add(posterThree, JLabel.CENTER); // CENTER DOESNT WORK
+			}
+		}
+
 		fourthResult.setSize(250, 75);
 		fourthResult.setBorder(blackBorder);
 		fourthResult.setBackground(purple);
@@ -124,24 +125,25 @@ public class ActorSearchResults {
 		fourthLabel = new JLabel(actorResultFour);
 		fourthLabel.setBackground(Color.blue);
 		fourthLabel.setHorizontalAlignment(JLabel.CENTER);
-		fourthResult.add(fourthLabel,BorderLayout.NORTH);
+		fourthResult.add(fourthLabel, BorderLayout.NORTH);
 		fourthResult.setVisible(true);
-		
-		
-		JButton button4 = new JButton("View " + results[3].getName());
-		button4.setSize(50, 10);
-		button4.setHorizontalAlignment(JButton.CENTER);
-		fourthResult.add(button4, BorderLayout.SOUTH);
-		
-		button4.addActionListener(e -> {
-			//try {
-				//MainGUI.changePanel(MovieDisplay.displayMovie(DataBase.SearchMovieByID(results[3].getID())));
-			//} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				//e1.printStackTrace();
-			//}
-		});
-		
+
+		JLabel posterFour = new JLabel();
+		if (results[3].getImage() != null) {
+			try {
+				URL posterLinkFour = new URL(results[3].getImage());
+				ImageIcon posterImageFour = new ImageIcon(posterLinkFour);
+				posterFour.setIcon(new ImageIcon(normalizeImage(posterImageFour)));
+				posterFour.setHorizontalAlignment(SwingConstants.TRAILING);
+				fourthResult.add(posterFour, JLabel.CENTER); // CENTER DOESNT WORK
+			} catch (MalformedURLException e) {
+				Image icon = (new ImageIcon("NoImageLink.png")).getImage();
+				posterFour.setIcon(new ImageIcon(icon));
+				posterFour.setHorizontalAlignment(SwingConstants.TRAILING);
+				fourthResult.add(posterFour, JLabel.CENTER); // CENTER DOESNT WORK
+			}
+		}
+
 		/*
 		 * fifthResult.setSize(250, 75); fifthResult.setBorder(blackBorder);
 		 * fifthResult.setBackground(purple); String actorResultFive =
@@ -179,5 +181,16 @@ public class ActorSearchResults {
 		 */
 
 		return mainPanel;
+	}
+
+	private static Image normalizeImage(ImageIcon image) {
+
+		Dimension dimensions = MainGUI.screenSize;
+		double idealHeight = dimensions.height * .3;
+		double idealWidth = (idealHeight / image.getIconHeight()) * image.getIconWidth();
+		System.out.println(dimensions);
+		System.out.println(idealWidth + " " + idealHeight);
+
+		return image.getImage().getScaledInstance((int) idealWidth, (int) idealHeight, Image.SCALE_SMOOTH);
 	}
 }
